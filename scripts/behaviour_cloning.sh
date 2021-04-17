3221703de87ca537fc8a0dfdba5a52601e7955f3
TortillasAlfred <mathieu.godbout.3@ulaval.ca>
2021-04-17 13:53:03 -0400

    No more cuda loading maybe ??

--- scripts/behaviour_cloning.sh
+++ scripts/behaviour_cloning.sh
@@ -15,6 +15,4 @@ export PYTHONPATH=/home/magod/git/ift7020-offlineRL/:$PYTHONPATH
 cp /scratch/magod/opti_combi/datasets.tar.gz $SLURM_TMPDIR
 tar -xzf $SLURM_TMPDIR/datasets.tar.gz -C $SLURM_TMPDIR/
 
-module load cuda
-
 python -u prepare_rl_training.py --train_bc 1 --working_path $SLURM_TMPDIR --saving_path /scratch/magod/opti_combi/results --job_index $SLURM_ARRAY_TASK_ID
